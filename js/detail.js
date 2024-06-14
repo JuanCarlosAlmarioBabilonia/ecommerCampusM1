e83b20ca4949416f62477d01d606f778cf13589d
JuanCarlosAlmarioBabilonia <almariobabilonia@gmail.com>
2024-06-14 17:06:03 -0500

    feat: :construction: Trabajo hecho en review
    Exportacion de titleProductDetail y creacion de main__section__title como variable

--- js/detail.js
+++ js/detail.js
@@ -1,13 +1,16 @@
 import { galleryCategory } from "./components/gallery.js";
+import { titleProductDetail } from "./components/section.js";
 import { getProductId } from "./module/detail.js";
 
 let main__section__gallery = document.querySelector("#main__section__gallery");
+let main__section__title = document.querySelector("#main__section__title")
 
 addEventListener("DOMContentLoaded", async(e)=>{
     let params = new URLSearchParams(location.search);
     let id = params.get('id');
     if(!localStorage.getItem(id)) localStorage.setItem(id, JSON.stringify(await getProductId({id})));
     main__section__gallery.innerHTML = await galleryCategory(JSON.parse(localStorage.getItem(id)))
+    main__section__title.innerHTML = await titleProductDetail(JSON.parse(localStorage.getItem(id)))
     // let {data} = res;
     // let {
     //     category_path,
